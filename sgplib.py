28f3f2b3bde06764a648753c8a597d6ac19eb3e8
tanhanze <tanhanze@qq.com>
2023-04-30 11:47:27 +0800

    Format lib

--- sgplib.py
+++ sgplib.py
@@ -1,49 +1,57 @@
-import zlib,socket
+import zlib
+
+
 class Package:
     def __init__(self):
         pass
-    def encode(self,content,trytozip=True,zip_level=6):
+
+    def encode(self, content, trytozip=True, zip_level=6):
         if trytozip:
-            zipped=zlib.compress(content,zip_level)
-            self.raw=content
-            self.zipped=zipped
+            zipped = zlib.compress(content, zip_level)
+            self.raw = content
+            self.zipped = zipped
         else:
-            self.raw=content
-            self.zipped=content
-        shouldzip=self.raw.__sizeof__() > self.zipped.__sizeof__()
+            self.raw = content
+            self.zipped = content
+        shouldzip = self.raw.__sizeof__() > self.zipped.__sizeof__()
         if shouldzip:
-            data=self.zipped
+            data = self.zipped
         else:
-            data=self.raw
-        self.zipped=shouldzip
-        self.data=bytes([int(shouldzip)])+data
-        
+            data = self.raw
+        self.zipped = shouldzip
+        self.data = bytes([int(shouldzip)])+data
+
         return self.data
-    def decode(self,content):
-        cont=list(content)
-        zipped=bool(cont[0])
-        string=bytes(cont[1:])
-        self.zipped=zipped
+
+    def decode(self, content):
+        cont = list(content)
+        zipped = bool(cont[0])
+        string = bytes(cont[1:])
+        self.zipped = zipped
         if zipped:
-            inside=zlib.decompress(string)
+            inside = zlib.decompress(string)
         else:
-            inside=string
-        self.raw=inside
+            inside = string
+        self.raw = inside
         return inside
 
+
 class Socket:
-    def __init__(self,socket):
-        self.socket=socket
-    def recv(self,size):
-        data=self.socket.recv(size)
+    def __init__(self, socket):
+        self.socket = socket
+
+    def recv(self, size):
+        data = self.socket.recv(size)
         if not data:
             raise IOError("Socket closed.")
-        temp=Package()
+        temp = Package()
         return temp.decode(data)
-    def send(self,data,ziplevel=6):
-        temp=Package()
-        zipped=temp.encode(data,ziplevel)
+
+    def send(self, data, ziplevel=6):
+        temp = Package()
+        zipped = temp.encode(data, ziplevel)
         return self.socket.send(zipped)
+
     @property
     def fileno(self):
         return self.socket.fileno
